a9f21812faf1f5055e8cad0ef288223e1f27414c
Andrey Kumykov <aakumykov@yandex.ru>
2023-11-13 10:51:28 +0300

    Добавил в меню кнопку свойств приложения.

--- app/src/main/java/com/github/aakumykov/kotlin_playground/MainActivity.kt
+++ app/src/main/java/com/github/aakumykov/kotlin_playground/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.github.aakumykov.kotlin_playground
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.github.aakumykov.file_lister.FSItem
@@ -22,5 +27,26 @@ class MainActivity : AppCompatActivity() {
             .beginTransaction()
             .replace(R.id.fragmentContainerView, MainFragment.create(), MainFragment.TAG)
             .commit()
+
+        setSupportActionBar(binding.toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (R.id.actionAppProperties == item.itemId) {
+            openAppPeoperties()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun openAppPeoperties() {
+        val uri = Uri.parse("package:$packageName")
+        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, uri)
+        startActivity(intent)
     }
 }
