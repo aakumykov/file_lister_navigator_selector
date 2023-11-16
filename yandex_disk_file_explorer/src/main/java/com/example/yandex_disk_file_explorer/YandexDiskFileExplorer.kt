738ea2b6deeee548f1cf39dde592e36c2d0273a6
Andrey Kumykov <aakumykov@yandex.ru>
2023-11-16 13:56:41 +0300

    Наполнил класс YandexDiskFileExplorer.

---
+++ yandex_disk_file_explorer/src/main/java/com/example/yandex_disk_file_explorer/YandexDiskFileExplorer.kt
@@ -0,0 +1,15 @@
+package com.example.yandex_disk_file_explorer
+
+import com.example.yandex_disk_file_lister.YandexDiskFileLister
+import com.github.aakumykov.file_explorer.BasicFileExplorer
+import com.github.aakumykov.file_explorer.FileExplorer
+import com.github.aakumykov.file_lister.FSItem
+import com.github.aakumykov.file_lister.FileLister
+
+class YandexDiskFileExplorer(
+    initialPath: String,
+    yandexDiskFileLister: YandexDiskFileLister,
+    listCache: FileLister.ListCache,
+    pathCache: FileExplorer.PathCache
+)
+    : BasicFileExplorer(initialPath, listCache, pathCache), FileLister by yandexDiskFileLister
