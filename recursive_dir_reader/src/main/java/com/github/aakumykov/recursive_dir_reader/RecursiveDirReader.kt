88b3711ed2ce3e0b414d2503e33f9df88de9bc7c
Andrey Kumykov <aakumykov@yandex.ru>
2023-11-29 20:40:32 +0300

    Модуль recursive_dir_reader с классом RecursiveDirReader.

---
+++ recursive_dir_reader/src/main/java/com/github/aakumykov/recursive_dir_reader/RecursiveDirReader.kt
@@ -0,0 +1,67 @@
+package com.github.aakumykov.recursive_dir_reader
+
+import com.github.aakumykov.file_lister.FileLister
+import java.io.File
+
+class RecursiveDirReader(private val fileLister: FileLister) {
+
+    private val list: MutableList<FileListItem> = mutableListOf()
+
+    fun readDir(initialPath: String): List<FileListItem> {
+
+        list.add(FileListItem(initialPath))
+
+        while(hasUnlistedDirs()) {
+
+            getUnlistedDir()?.let { currentlyListedDir: FileListItem ->
+
+                fileLister.listDir(currentlyListedDir.absolutePath).forEach { fsItem ->
+
+                    val childItem = FileListItem(
+                        file = File(currentlyListedDir, fsItem.name),
+                        parentDir = currentlyListedDir
+                    )
+
+                    currentlyListedDir.addChildId(childItem.id)
+
+                    list.add(childItem)
+                }
+
+                currentlyListedDir.isListed = true
+            }
+        }
+
+        return list
+    }
+
+    private fun hasUnlistedDirs(): Boolean {
+        return null != getUnlistedDir()
+    }
+
+    private fun getUnlistedDir(): FileListItem? {
+        return list.firstOrNull { item -> (item.isDirectory && !item.isListed) }
+    }
+
+
+    // TODO: унаследовать FileListItem от FSItem?
+    class FileListItem (
+        absolutePath: String,
+        val parentId: String? = null,
+        val childIds: MutableList<String> = mutableListOf(),
+        var isListed: Boolean = false
+    )
+        : File(absolutePath)
+    {
+        constructor(file: File, parentDir: FileListItem) : this(absolutePath = file.absolutePath, parentId = parentDir.id)
+
+        val id: String get() = absolutePath
+
+        fun addChildId(id: String) {
+            childIds.add(id)
+        }
+    }
+
+    companion object {
+        val TAG: String = RecursiveDirReader::class.java.simpleName
+    }
+}
