45b04d1e075435e993fb62593f15eeaba0bad758
Andrey Kumykov <aakumykov@yandex.ru>
2023-12-06 18:30:03 +0300

    Пометил конструктор YandexDiskFileLister как @AssistedInject.

--- yandex_disk_file_lister/src/main/java/com/github/aakumykov/yandex_disk_file_lister/YandexDiskFileLister.kt
+++ yandex_disk_file_lister/src/main/java/com/github/aakumykov/yandex_disk_file_lister/YandexDiskFileLister.kt
@@ -1,10 +1,16 @@
 package com.github.aakumykov.yandex_disk_file_lister
 
-import com.github.aakumykov.fs_item.FSItem
 import com.github.aakumykov.file_lister.FileLister
+import com.github.aakumykov.fs_item.FSItem
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
 
-class YandexDiskFileLister(authToken: String, private val dirSeparator: String = FSItem.DS) : FileLister {
-
+class YandexDiskFileLister @AssistedInject constructor(
+    @Assisted authToken: String,
+    private val dirSeparator: String = FSItem.DS
+)
+    : FileLister
+{
     private val yandexDiskClient: FileListerYandexDiskClient by lazy {
         FileListerYandexDiskClient(authToken)
     }
