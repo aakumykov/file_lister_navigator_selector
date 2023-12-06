9c5a09335fc07d76e6642329160ee1c3c2e6330c
Andrey Kumykov <aakumykov@yandex.ru>
2023-12-06 20:41:55 +0300

    Revert "Пометил конструктор LocalFileLister как @AssistedInject."
    This reverts commit 28e1904f17eec8e9f86810744838706c39cdfc12.

--- local_file_lister/src/main/java/com/github/aakumykov/local_file_lister/LocalFileLister.kt
+++ local_file_lister/src/main/java/com/github/aakumykov/local_file_lister/LocalFileLister.kt
@@ -3,15 +3,10 @@ package com.github.aakumykov.local_file_lister
 import com.github.aakumykov.fs_item.FSItem
 import com.github.aakumykov.file_lister.FileLister
 import com.github.aakumykov.fs_item.SimpleFSItem
-import dagger.assisted.AssistedInject
 import java.io.File
-import javax.inject.Inject
 
-class LocalFileLister @AssistedInject constructor(
-    private val dirSeparator: String = FSItem.DS
-)
-    : FileLister
-{
+class LocalFileLister(private val dirSeparator: String = FSItem.DS) : FileLister {
+
     override fun listDir(path: String): List<FSItem> {
 
         val fileNamesArray = File(path).list()
