0a95c54bde90c8594c591fad11a55951b54642bb
Andrey Kumykov <aakumykov@yandex.ru>
2023-12-08 15:37:16 +0300

    Добавил в LocalFileLister @Assisted-параметр (поле)-заглушку (dummyAuthToken).

--- local_file_lister/src/main/java/com/github/aakumykov/local_file_lister/LocalFileLister.kt
+++ local_file_lister/src/main/java/com/github/aakumykov/local_file_lister/LocalFileLister.kt
@@ -1,12 +1,18 @@
 package com.github.aakumykov.local_file_lister
 
+import android.webkit.PluginStub
 import com.github.aakumykov.fs_item.FSItem
 import com.github.aakumykov.file_lister.FileLister
 import com.github.aakumykov.fs_item.SimpleFSItem
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
 import java.io.File
 
-class LocalFileLister(private val dirSeparator: String = FSItem.DS) : FileLister {
-
+class LocalFileLister @AssistedInject constructor(
+    @Assisted private val dummyAuthToken: String
+)
+    : FileLister
+{
     override fun listDir(path: String): List<FSItem> {
 
         val fileNamesArray = File(path).list()
@@ -15,7 +21,7 @@ class LocalFileLister(private val dirSeparator: String = FSItem.DS) : FileLister
 
         if (null != fileNamesArray) {
             for (name: String in fileNamesArray) {
-                val absolutePath = path + dirSeparator + name
+                val absolutePath = path + FSItem.DS + name
                 val file = File(absolutePath)
                 val simpleFSItem = SimpleFSItem(file)
                 fileList.add(simpleFSItem)
