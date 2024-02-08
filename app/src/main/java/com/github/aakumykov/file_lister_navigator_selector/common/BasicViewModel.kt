4401ffe3a92b40295bb142b8ed21fae40431b5df
Andrey Kumykov <aakumykov@yandex.ru>
2024-02-08 11:39:17 +0300

    BasicViewModel имеет поля LiveData с FileSortingMode.

--- app/src/main/java/com/github/aakumykov/file_lister_navigator_selector/common/BasicViewModel.kt
+++ app/src/main/java/com/github/aakumykov/file_lister_navigator_selector/common/BasicViewModel.kt
@@ -3,17 +3,28 @@ package com.github.aakumykov.file_lister_navigator_selector.common
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.github.aakumykov.file_lister_navigator_selector.file_lister.FileSortingMode
 import com.github.aakumykov.file_lister_navigator_selector.fs_item.FSItem
 import com.github.aakumykov.file_lister_navigator_selector.fs_navigator.FileExplorer
 
 abstract class BasicViewModel : ViewModel(), FileExplorer.ListCache, FileExplorer.PathCache {
 
+    protected val _currentSortingMode: MutableLiveData<FileSortingMode> = MutableLiveData(DEFAULT_SORTING_MODE)
+    val sortingModeLiveData: LiveData<FileSortingMode> = _currentSortingMode
+
+
     protected val _currentList: MutableLiveData<List<FSItem>> = MutableLiveData()
-    val currentList: LiveData<List<FSItem>> = _currentList
+    val listLiveData: LiveData<List<FSItem>> = _currentList
+
 
     protected val _currentPath: MutableLiveData<String> = MutableLiveData()
     val currentPath: LiveData<String> = _currentPath
 
 
     abstract fun getFileExplorer(): FileExplorer
+
+
+    companion object {
+        val DEFAULT_SORTING_MODE = FileSortingMode.NAME_DIRECT
+    }
 }
