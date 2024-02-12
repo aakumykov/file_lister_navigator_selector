c0809931d80a9da6edffe2312c47137a5e0330d7
Andrey Kumykov <aakumykov@yandex.ru>
2024-02-12 15:36:26 +0300

    Добавил файлы StorageAccessHelper-а.

---
+++ app/src/main/java/com/github/aakumykov/file_lister_navigator_selector/storage_access_helper/StorageAccessHelper.kt
@@ -0,0 +1,29 @@
+package com.github.aakumykov.file_lister_navigator_selector.storage_access_helper
+
+import android.os.Build
+import androidx.fragment.app.FragmentActivity
+
+interface StorageAccessHelper {
+
+    fun requestStorageAccess(resultCallback: (isGranted: Boolean) -> Unit)
+
+    @Deprecated("Используй hasStorageReadingAccess(), hasStorageWritingAccess")
+    fun hasStorageAccess(): Boolean
+
+    fun hasStorageReadingAccess(): Boolean
+    fun hasStorageWritingAccess(): Boolean
+
+    fun openStorageAccessSettings()
+
+
+    companion object {
+        fun create(componentActivity: FragmentActivity): StorageAccessHelper {
+            return when {
+                isAndroidROrLater() -> StorageAccessHelperModern(componentActivity)
+                else -> StorageAccessHelperLegacy(componentActivity)
+            }
+        }
+
+        private fun isAndroidROrLater() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.R
+    }
+}
---
+++ app/src/main/java/com/github/aakumykov/file_lister_navigator_selector/storage_access_helper/StorageAccessHelperLegacy.kt
@@ -0,0 +1,48 @@
+package com.github.aakumykov.file_lister_navigator_selector.storage_access_helper
+
+import android.content.pm.PackageManager
+import androidx.fragment.app.FragmentActivity
+import permissions.dispatcher.ktx.PermissionsRequester
+import permissions.dispatcher.ktx.constructPermissionsRequest
+
+class StorageAccessHelperLegacy(private val activity: FragmentActivity): StorageAccessHelper {
+
+    private val readingStoragePermissionsRequester: PermissionsRequester
+    private var resultCallback: ((isGranted: Boolean) -> Unit)? = null // TODO: в интерфейс...
+
+    init {
+        readingStoragePermissionsRequester = activity.constructPermissionsRequest(
+            android.Manifest.permission.READ_EXTERNAL_STORAGE,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            requiresPermission = { resultCallback?.invoke(true) },
+            onPermissionDenied = { resultCallback?.invoke(false) },
+            onNeverAskAgain = { resultCallback?.invoke(true) }
+        )
+    }
+
+    override fun requestStorageAccess(resultCallback: (isGranted: Boolean) -> Unit) {
+        this.resultCallback = resultCallback
+        readingStoragePermissionsRequester.launch()
+    }
+
+    override fun hasStorageAccess(): Boolean {
+        return PackageManager.PERMISSION_GRANTED == activity.checkCallingOrSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+    }
+
+    override fun hasStorageReadingAccess(): Boolean {
+        return isPermissionGranted(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+    }
+
+    override fun hasStorageWritingAccess(): Boolean {
+        return isPermissionGranted(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    }
+
+    override fun openStorageAccessSettings() {
+        IntentHelper.appSettingsIntent(activity)
+    }
+
+
+    private fun isPermissionGranted(usesPermissionString: String): Boolean {
+        return PackageManager.PERMISSION_GRANTED == activity.checkCallingOrSelfPermission(usesPermissionString)
+    }
+}
