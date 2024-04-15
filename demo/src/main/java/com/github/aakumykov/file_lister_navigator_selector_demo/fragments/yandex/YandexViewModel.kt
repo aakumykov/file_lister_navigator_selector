6ac7951ebba41b05e433f36e1686a9362c235bbf
Andrey Kumykov <aakumykov@yandex.ru>
2024-04-15 21:02:30 +0300

    Модуль demo вместо app с одновременным изменением пространства имён для решения проблемы с не найденными ресурсами.

---
+++ demo/src/main/java/com/github/aakumykov/file_lister_navigator_selector_demo/fragments/yandex/YandexViewModel.kt
@@ -0,0 +1,43 @@
+package com.github.aakumykov.file_lister_navigator_selector_demo.fragments.yandex
+
+import android.os.Handler
+import android.os.Looper
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.github.aakumykov.file_lister_navigator_selector.file_explorer.FileExplorer
+import com.github.aakumykov.file_lister_navigator_selector.file_lister.SimpleSortingMode
+import com.github.aakumykov.file_lister_navigator_selector.fs_item.FSItem
+
+// TODO: сделать SimpleSortingMode параметром типа
+class YandexViewModel : ViewModel(), FileExplorer.ListCache, FileExplorer.PathCache {
+
+    private val handler = Handler(Looper.getMainLooper())
+
+
+    private var _fileExplorer: FileExplorer<SimpleSortingMode>? = null
+
+    val fileExplorer get() = _fileExplorer!!
+
+    fun setFileExplorer(fileExplorer: FileExplorer<SimpleSortingMode>) {
+        _fileExplorer = fileExplorer
+    }
+
+
+    private val _listMutableLiveData: MutableLiveData<List<FSItem>> = MutableLiveData()
+    val currentList: LiveData<List<FSItem>> get() = _listMutableLiveData
+
+
+    private val _pathMutableLiveData: MutableLiveData<String> = MutableLiveData()
+    val currentPath: LiveData<String> get() = _pathMutableLiveData
+
+
+    override fun cachePath(path: String) {
+        handler.post { _pathMutableLiveData.value = path }
+    }
+
+
+    override fun cacheList(list: List<FSItem>) {
+        handler.post { _listMutableLiveData.value = list }
+    }
+}
