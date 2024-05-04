2f538a316e2d3c7a5164879689f8c6d42baa6c4e
Andrey Kumykov <aakumykov@yandex.ru>
2024-05-04 11:06:52 +0300

    fix: перепутанный прямой/обратный режим сортировки по имени; v0.0.21

--- yandex_disk/src/main/java/com/github/aakumykov/yandex_disk_file_lister_navigator_selector/yandex_disk_file_lister/FileListerYandexDiskClient.kt
+++ yandex_disk/src/main/java/com/github/aakumykov/yandex_disk_file_lister_navigator_selector/yandex_disk_file_lister/FileListerYandexDiskClient.kt
@@ -1,5 +1,6 @@
 package com.github.aakumykov.yandex_disk_file_lister_navigator_selector.yandex_disk_file_lister
 
+import android.util.Log
 import com.github.aakumykov.file_lister_navigator_selector.file_lister.SimpleSortingMode
 import com.github.aakumykov.file_lister_navigator_selector.fs_item.FSItem
 import com.github.aakumykov.file_lister_navigator_selector.fs_item.SimpleFSItem
@@ -12,8 +13,11 @@ class FileListerYandexDiskClient(authToken: String)
     : YandexDiskClient<FSItem, SimpleSortingMode>(authToken)
 {
     override fun appToDiskSortingMode(appSortingMode: SimpleSortingMode, reverseOrder: Boolean): YandexDiskSortingMode {
+
+        Log.d(TAG, "appToDiskSortingMode(), mode: $appSortingMode, reverseOrder: $reverseOrder")
+
         return when(appSortingMode) {
-            SimpleSortingMode.NAME -> if (reverseOrder) YandexDiskSortingMode.NAME_DIRECT else YandexDiskSortingMode.NAME_REVERSE
+            SimpleSortingMode.NAME -> if (reverseOrder) YandexDiskSortingMode.NAME_REVERSE else YandexDiskSortingMode.NAME_DIRECT
             SimpleSortingMode.C_TIME -> if (reverseOrder) YandexDiskSortingMode.C_TIME_FROM_OLD_TO_NEW else YandexDiskSortingMode.C_TIME_FROM_NEW_TO_OLD
             SimpleSortingMode.M_TIME -> if (reverseOrder) YandexDiskSortingMode.M_TIME_FROM_OLD_TO_NEW else YandexDiskSortingMode.M_TIME_FROM_NEW_TO_OLD
             SimpleSortingMode.SIZE -> if (reverseOrder) YandexDiskSortingMode.SIZE_FROM_SMALL_TO_BIG else YandexDiskSortingMode.SIZE_FROM_BIG_TO_SMALL
@@ -37,5 +41,9 @@ class FileListerYandexDiskClient(authToken: String)
 
     override fun cloudFileToString(resource: Resource): String {
         return "Yandex Disk item '${resource.name}'"
+    }
+
+    companion object {
+        val TAG: String = FileListerYandexDiskClient::class.java.simpleName
     }
 }
